5e7d776faabe4721c2d3467c6a948490c8aef1ab
Ewo Jima <ll-EwoJima-ll@outlook.com>
2024-08-04 00:00:02 -0400

    :)

--- server/ServerThreaded.cpp
+++ server/ServerThreaded.cpp
@@ -1,149 +1,94 @@
 #include <iostream>
-#include <thread>
+#include <WinSock2.h>
 #include <vector>
-#include <memory>
-#include <winsock2.h>
-#include <mutex>
-#include <tchar.h>
-#include <ws2tcpip.h>
+#include <thread>
 
-// Line 38
+#pragma comment(lib, "Ws2_32.lib")
+#pragma warning(disable:4996) 
 
-#define MAX_CLIENTS 10
-#define MESSAGE_BUFFER_SIZE 1024
+#define MessageLength 1024
+#define MaxClients 10
 
-#pragma comment(lib,"Ws2_32.lib")
+int RecvMessages(SOCKET Socket, std::vector<SOCKET> Clients) {
+	// Get The Message
+	char Message[MessageLength];
+	int Bytes = recv(Socket, Message, MessageLength, 0);
+	int MessageLen = sizeof(Message);
 
-using std::cout;
+	// Send The Message To All OTher Clients
+	if (Bytes != 0) {
+		std::cout << "Client: " << Message << std::endl;
+		for (SOCKET Client : Clients) {
+			if (Socket != Client) {
+				send(Socket, Message, MessageLen, 0);
+			}
+		}
+	}
 
-std::mutex clientMutex;
-
-void getMessages(SOCKET client, std::vector<SOCKET>& clientSockets) {
-    char buffer[MESSAGE_BUFFER_SIZE];
-
-    while (true) {
-        int byteCount = recv(client, buffer, MESSAGE_BUFFER_SIZE, 0);
-
-        if (byteCount <= 0) {
-            if (byteCount == 0) {
-                std::cout << "Connection closed by client" << std::endl;
-            }
-            else {
-                std::cout << "recv failed: " << WSAGetLastError() << std::endl;
-            }
-            closesocket(client);
-            break;
-        }
-
-        //buffer[byteCount] = '\0'; // Buffer overflow problem
-        printf("Client: %s\n", buffer);
-
-        std::lock_guard<std::mutex> lock(clientMutex);
-        for (SOCKET otherClient : clientSockets) {
-            if (otherClient != client) {
-                send(otherClient, buffer, byteCount, 0);
-            }
-        }
-    }
+	return 0;
 }
 
-SOCKET makeClientConnection(const SOCKET serverSocket) {
-    SOCKET acceptSocket = accept(serverSocket, NULL, NULL);
+SOCKET NewSocket() { // This Being In A Function Is Now Pointless, But Can Be Left Alone
+	// Create A Socket
+	SOCKET Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (Socket == INVALID_SOCKET) {
+		std::cout << "Socket Failed With Error: " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	} else {
+		std::cout << "Connection Succesful" << std::endl;
+	}
 
-    if (acceptSocket == INVALID_SOCKET) {
-        std::cout << "Accept failed: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return INVALID_SOCKET;
-    }
-
-    return acceptSocket;
+	return Socket;
 }
 
-void connectionManager(const SOCKET server, std::vector<std::unique_ptr<std::thread>>& clientThreads, std::vector<SOCKET>& clientSockets) {
-    SOCKET clientSocket = makeClientConnection(server);
-    clientSockets.push_back(clientSocket);
+void ConnectionManager() { // Put In Thread
+	std::cout << "Listening For Connection's" << std::endl;
 
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        std::lock_guard<std::mutex> lock(clientMutex);
-        if (clientThreads[i] == nullptr || !clientThreads[i]->joinable()) {
+	// Get The Number Of Clients In The Client Vector
+	std::vector<SOCKET> Clients(MaxClients);
+	int ClientCount;
 
-            if (clientSocket != INVALID_SOCKET) {
-                clientThreads[i] = std::make_unique<std::thread>(getMessages, clientSocket, std::ref(clientSockets));
-            }
-            break;
-        }
-    }
+	// Listen For Connections
+	while (true) {
+		SOCKET Socket = INVALID_SOCKET;
+		SOCKET Client = accept(Socket, NULL, NULL);
+		Clients.push_back(Client);
+
+		// Get Messages From Client
+		int Status = RecvMessages(Socket, Clients);
+		if (Status == 0) {
+			continue;
+
+		} else {
+			std::cout << "Connection Suddenly Stopped" << std::endl;
+			std::terminate(); // STOP THREAD
+		}
+	}
 }
 
-SOCKET makeServer() {
-    const int port = 55555;
-    int wsaerr;
-    WORD wVersionRequested = MAKEWORD(2, 2);
-    SOCKET serverSocket;
-    WSADATA wsaData;
+int main() {
+	// Initialize WSA
+	int WSAResult;
+	WSADATA WSAData;
 
-    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+	SOCKET connect = INVALID_SOCKET;
 
-    if (wsaerr != 0) {
-        cout << "The winsock dll not found!" << std::endl;
-        return 0;
-    }
+	WSAResult = WSAStartup(MAKEWORD(2, 2), &WSAData);
+	if (WSAResult != NO_ERROR) {
+		std::cout << "WSA Startup With Error: " << WSAResult << std::endl;
+		return 1;
+	}
 
-    serverSocket = INVALID_SOCKET;
-    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	// Setup the Socket Information
+	struct sockaddr_in SockInfo;
+	SockInfo.sin_family = AF_INET;
+	SockInfo.sin_addr.s_addr = inet_addr("192.168.1.152");
+	SockInfo.sin_port = htons(55555);
 
-    if (serverSocket == INVALID_SOCKET) {
-        cout << "Error at socket(): " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 0;
-    }
+	// Manage The Connection
+	std::thread obj(ConnectionManager);
+	obj.join();
 
-    sockaddr_in service;
-    service.sin_family = AF_INET;
-
-    InetPton(AF_INET, _T("192.168.56.1"), &service.sin_addr.s_addr);
-    service.sin_port = htons(port);
-
-    if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-        cout << "bind() failed: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 0;
-    }
-
-    if (listen(serverSocket, 1) == SOCKET_ERROR) {
-        cout << "listen(): Error listening on socket " << WSAGetLastError << std::endl;
-        WSACleanup();
-        return 0;
-    }
-
-    return serverSocket;
-}
-
-int main(void) {
-    std::vector<std::unique_ptr<std::thread>> clientThreads(MAX_CLIENTS);
-    std::vector<SOCKET> clientSockets(MAX_CLIENTS);
-
-    SOCKET server = makeServer();
-    if (server == INVALID_SOCKET) {
-        return 1;
-    }
-
-    while (true) {
-        connectionManager(server, clientThreads, clientSockets);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-
-    for (auto& clientThread : clientThreads) {
-        if (clientThread && clientThread->joinable()) {
-            clientThread->join();
-        }
-    }
-
-    for (SOCKET clientSocket : clientSockets) {
-        closesocket(clientSocket);
-    }
-
-    closesocket(server);
-    WSACleanup();
-    return 0;
+	return 0;
 }
